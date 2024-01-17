b9db51a89d825e40bb7d22eeaee2d592ab456266
Yash Verma <yashv1752@gmail.com>
2024-01-18 02:31:30 +0530

    first commit

---
+++ server/routes/questions.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import { AskQuestion, getAllQuestions, deleteQuestion,voteQuestion } from '../controllers/questions.js'
+
+
+const router = express.Router()
+
+router.post('/Ask' ,AskQuestion)
+router.get('/get', getAllQuestions)
+router.delete('/delete/:id' ,deleteQuestion)
+router.patch('/vote/:id' ,voteQuestion)
+
+export default router
